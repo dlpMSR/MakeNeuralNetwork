752ff5b2fa7e2543329117086add602abe67707a
DESKTOP-NUB72QV\nagiclocal <pntsec_msr@outlook.com>
2018-06-01 12:00:12 +0900

    NNクラスの定義とインスタンスの生成

---
+++ neuralnetwork.py
@@ -0,0 +1,28 @@
+class newralNetwork:
+    def __init__(self, input_nodes, hidden_nodes,
+                 output_nodes, learning_rate):
+        self.inodes = input_nodes
+        self.hnodes = hidden_nodes
+        self.onodes = output_nodes
+        self.lr = learning_rate
+        pass
+
+    def train():
+        pass
+
+    def query():
+        pass
+
+
+def main():
+    input_nodes = 3
+    hidden_nodes = 3
+    output_nodes = 3
+    lerning_rate = 0.3
+    n = newralNetwork(input_nodes, hidden_nodes,
+                      output_nodes, learning_rate)
+    pass
+
+
+if __name__ == '__main__':
+    main()
