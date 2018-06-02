a3edb72a25558747ccd13addbd58b5215c257948
DLPMSR-PEACH\Zechs <pntsec_msr@outlook.com>
2018-06-02 23:01:54 +0900

    なんかした

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -2,6 +2,7 @@ import numpy
 import scipy.special
 
 class newralNetwork:
+
     def __init__(self, input_nodes, hidden_nodes,
                  output_nodes, learning_rate):
         self.inodes = input_nodes
@@ -15,6 +16,7 @@ class newralNetwork:
         self.activation_function = lambda x: scipy.special.expit(x)
         pass
 
+
     def train(self, inputs_list, targets_list):
         inputs = numpy.array(inputs_list, ndmin=2).T
         targets = numpy.array(targets_list, ndmin=2).T        
@@ -26,8 +28,15 @@ class newralNetwork:
         final_outputs = self.activation_function(final_inputs)
 
         output_errors = targets - final_outputs
+
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+
+        self.who += self.lr * numpy.dot(output_errors*final_outputs*(1.0-final_outputs),
+                                       numpy.transpose(hidden_outputs))
         
-        pass
+        self.wih = self.ir * numpy.dot(hidden_errors*hidden_outputs*(1.0-hidden_outputs),
+                                       numpy.transpose(inputs))
+                                    
 
     def query(self, inputs_list):
         inputs = numpy.array(inputs_list, ndmin=2).T
@@ -37,8 +46,9 @@ class newralNetwork:
 
         final_inputs = numpy.dot(self.who, hidden_outputs)
         final_outputs = self.activation_function(final_inputs)
-        pass
 
+        return final_outputs
+        
 
 def main():
     input_nodes = 3
